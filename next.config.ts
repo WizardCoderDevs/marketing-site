e8b372a226c0bc0429a28e76d6fa75689f658aa1
mracalf <antonio.araujol@outlook.com>
2025-06-10 21:31:59 -0300

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   // Enable React strict mode for better development experience
   reactStrictMode: true,
 
